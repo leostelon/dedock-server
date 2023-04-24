bd966abe804994ddfcd754b9af5c69d758b76aff
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2023-04-25 01:07:11 +0530

    basic upload

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,11 +1,51 @@
 const path = require("path");
 const router = require("express").Router();
 const { SpheronClient, ProtocolEnum } = require("@spheron/storage");
+const { upload } = require("../middlewares/multer");
 const token = "your-token";
 
 const client = new SpheronClient({ token });
+router.post(
+	"/upload",
+	upload.single("image"),
+	async (req, res) => {
+		try {
+			if (!req.file) {
+				return res.send({ message: "Send file" });
+			}
+			const file = req.file;
 
-router.get("/upload", async (req, res) => {
+			// Upload to Spheron
+			const filePath = path.join(__dirname, "../../uploads/" + file.filename);
+			const response = await client.upload(filePath, {
+				protocol: ProtocolEnum.IPFS,
+				name: "testbucketspriyo",
+				onUploadInitiated: (uploadId) => {
+					console.log(`Upload with id ${uploadId} started...`);
+				},
+				onChunkUploaded: (uploadedSize, totalSize) => {
+					let currentlyUploaded;
+					currentlyUploaded += uploadedSize;
+					console.log(`Uploaded ${currentlyUploaded} of ${totalSize} Bytes.`);
+				},
+			});
+
+			// Delete File
+			fs.rmSync(`${file.destination}/${file.filename}`);
+
+			res.send(response);
+		} catch (error) {
+			console.log(error);
+			res.status(500).send({ message: error.message });
+		}
+	},
+	(err, req, res, next) => {
+		console.log(err);
+		res.status(400).send({ error: err.message });
+	}
+);
+
+router.get("/spheronfileuploadcheck", async (req, res) => {
 	try {
 		const filepath = path.join(__dirname, "./upload.txt");
 		console.log(filepath);
@@ -16,6 +56,7 @@ router.get("/upload", async (req, res) => {
 				console.log(`Upload with id ${uploadId} started...`);
 			},
 			onChunkUploaded: (uploadedSize, totalSize) => {
+				let currentlyUploaded;
 				currentlyUploaded += uploadedSize;
 				console.log(`Uploaded ${currentlyUploaded} of ${totalSize} Bytes.`);
 			},
