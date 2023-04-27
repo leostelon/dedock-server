0878c840475057fe567720bd9c83d4087797dbb8
leostelon <53221136+leostelon@users.noreply.github.com>
2023-04-27 23:03:56 +0530

    added username based arch 1

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const { recoverPersonalSignature } = require("@metamask/eth-sig-util");
-const { createUser } = require("../polybase/user");
+const { createUser, getUser } = require("../polybase/user");
+const { auth } = require("../middlewares/auth");
+const { db } = require("../polybase");
+const collectionReference = db.collection("User");
 
 router.post("/user/login", async (req, res) => {
     try {
@@ -13,6 +16,36 @@ router.post("/user/login", async (req, res) => {
 
         let token = await createUser(recoveredAddress);
         res.send({ token })
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+})
+
+router.post("/user/username", auth, async (req, res) => {
+    try {
+        const { username } = req.body;
+        const usernameRegex = /^[a-z0-9_\.]+$/;
+        const valid = usernameRegex.test(username)
+        if (!valid) return res.status(500).send({ message: "Invalid username." })
+
+        const response = await collectionReference
+            .record(req.user.id)
+            .call("updateUsername", [username]);
+
+        res.send(response)
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+})
+
+router.get("/user/:address", async (req, res) => {
+    try {
+        const { address } = req.params;
+
+        const response = await getUser(address)
+        delete response.data.token;
+
+        res.send(response.data)
     } catch (error) {
         res.status(500).send({ message: error.message })
     }
